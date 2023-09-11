9fb1f20c74488071cc9d582cb03a3e7983152de0
Michael Steinmetz <michael.steinmetz@vico-consulting.com>
2023-09-11 14:45:12 +0200

    mkfifo to get pid of first

--- startrun.sh
+++ startrun.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-VDEV=$(v4l2-ctl --list-devices -k | grep -A 3 HD-5000 | grep /dev/video  | head -n1)
-echo $VDEV | grep -q /dev/video || exit
 
+VDEV=$(v4l2-ctl --list-devices -k | grep -A 3 HD-5000 | grep /dev/video  | head -n1)
+FR=20
+
+echo $VDEV | grep -q /dev/video || exit
 v4l2-ctl -d $VDEV -c focus_automatic_continuous=0
 sleep 1
 v4l2-ctl -d $VDEV -c focus_automatic_continuous=0,focus_absolute=37
-OUTFILE="$(/bin/date +"%Y-%m-%d-%H-%M-%S")-$RANDOM.mp4"
+OUTFILE="Out/$(/bin/date +"%Y-%m-%d-%H-%M-%S")-$RANDOM.mp4"
 
-# OVERLAY=" -vf \"drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf: text='\%T': fontcolor=white@0.8: x=7: y=460\""
-OVERLAY=""
-BESTIES=" -c:v libx264 -preset slow -crf 22 "
+FONT=/usr/share/fonts/adobe-source-code-pro/SourceCodePro-Medium.otf
+# OVERLAY="\"drawtext=fontfile=$FONT:fontsize=38:box=1:boxcolor=white@0.5:text='Time\: %{pts\:gmtime\:0\:%M\\\\\:%S}s'\""
+BESTIES=" -c:v libx264 -preset fast -crf 22 "
 
-ffmpeg -f v4l2 -framerate 25                                \
+mkfifo /tmp/mpegpipe 
+ffmpeg -f v4l2 -framerate $FR                               \
 	-video_size 1280x720 -input_format mjpeg            \
-         $OVERLAY \
-	 -i $VDEV -f tee -map 0 $BESTIES "$OUTFILE|[f=nut]pipe:" | ffplay pipe: 
-
+	 -i $VDEV -f tee -map 0 $BESTIES                    \
+         -vf "drawtext=fontfile=$FONT:fontsize=38:box=1:boxcolor=white@0.5:text='Time\: %{pts\:gmtime\:0\:%M\\\\\:%S}s'"         \
+	 "$OUTFILE|[f=nut]pipe:" > /tmp/mpegpipe & 
+	
+FF=$!
+ffplay /tmp/mpegpipe
+kill $FF
